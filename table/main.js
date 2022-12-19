ecb47fba6915f68b1a1ac5ce3e1166d729debd87
Admin <admin@Tanveers-MacBook-Air.local>
2022-12-19 19:21:42 +0530

    adding table program.

---
+++ table/main.js
@@ -0,0 +1,15 @@
+var n = parseInt(prompt("enter number"));
+/*for (let i=1;i<=10;i++) 
+{
+	document.write(i+"*"+n+"="+i*n+"<br>");
+}*/
+let i =1;
+/*while(i<=10){
+	document.write(i+"*"+n+"="+i*n+"<br>");
+	i++;
+}*/
+do{
+   document.write(i+"*"+n+"="+i*n+"<br>");	
+   i++;
+}
+while (i<=10);
